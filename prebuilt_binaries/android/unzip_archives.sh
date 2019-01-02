0c088badc5586cbc4747b4c01719fa40bc690385
Leo An <leo@innoaus.com>
2019-01-02 15:52:38 +0900

    add prebuilt binaries for android

---
+++ prebuilt_binaries/android/unzip_archives.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+unzip aarch64.zip
+unzip arm.zip
+unzip arm64-v8a.zip
+unzip armeabi-v7a.zip
+unzip armeabi.zip
+unzip armv5.zip
+unzip armv6.zip
+unzip armv7-a.zip
+unzip armv7.zip
+unzip armv7a-neon.zip
+unzip armv7a.zip
+unzip armv8.zip
+unzip armv8a.zip
+unzip mips.zip
+unzip mips64.zip
+unzip mips64el.zip
+unzip mipsel64.zip
+unzip neon.zip
+unzip x86.zip
+unzip x86_64.zip
